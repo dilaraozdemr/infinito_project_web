a3f985a8a157366676fa2d12d3a2088143d93739
dilaraozdmr <dilarozdmr66@gmail.com>
2024-05-01 16:59:36 +0300

    add file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -80,6 +80,7 @@ class _HomePageState extends State<HomePage> {
                         Container(child: Image.asset("assets/image/home.jpeg")),
                         Container(child: Image.asset("assets/image/home.jpeg")),
                         Container(child: Image.asset("assets/image/home.jpeg")),
+                        Container(child: Image.asset("assets/image/home.jpeg")),
                       ],
                     ),
                   ],
